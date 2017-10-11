f56777e34f70190791fc73e28e35ea14689ed2bd
Marc-Antoine FERNANDES <embraser01@gmail.com>
2017-10-11 09:33:25 +0200

    Add parsing lib

---
+++ platform/src/main/java/lhexanome/optimodlivraison/platform/parsing/common/LoadFile.java
@@ -0,0 +1,27 @@
+package lhexanome.optimodlivraison.platform.parsing.common;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LoadFile {
+
+    public static Element loadFromFile(File inputFile) throws JDOMException, IOException {
+
+        return load(new FileInputStream(inputFile));
+    }
+
+
+    public static Element load(InputStream inputStream) throws JDOMException, IOException {
+        SAXBuilder saxBuilder = new SAXBuilder();
+        Document document = saxBuilder.build(inputStream);
+
+        return document.getRootElement();
+    }
+}
