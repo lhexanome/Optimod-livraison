063203e4e7f501c50c41cc190dbb4c441ac36fab
loicsrz <ljedi007@gmail.com>
2017-10-17 09:19:17 +0200

    Add LivraisonTest

--- platform/src/test/java/lhexanome/optimodlivraison/platform/models/LivraisonTest.java
+++ platform/src/test/java/lhexanome/optimodlivraison/platform/models/LivraisonTest.java
@@ -2,6 +2,9 @@ package lhexanome.optimodlivraison.platform.models;
 
 import org.junit.jupiter.api.Test;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,4 +12,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LivraisonTest {
 
+    @Test
+    void shouldSetDuration(){
+        //With
+        Livraison deliveries = new Livraison();
+
+        //When
+        deliveries.setDuration(5236);
+
+        //Then
+        assertEquals(5236,deliveries.getDuration());
+    }
+
+    @Test
+    void shouldSetIntersection(){
+        //With
+        Livraison deliveries = new Livraison();
+        Intersection intersection = new Intersection();
+
+        //When
+        intersection.setId(1234);
+        deliveries.setIntersection(intersection);
+
+        //Then
+        assertEquals(intersection,deliveries.getIntersection());
+    }
+
+    @Test
+    void shouldSetPlageHoraire() throws ParseException {
+        //With
+        Livraison deliveries = new Livraison();
+        DateFormat dateFormat = new SimpleDateFormat("DD-MM-YY HH:mm");
+        Date start = dateFormat.parse("15-02-17 12:55");
+        Date end = dateFormat.parse("15-02-17 10:55");
+        PlageHoraire slot = new PlageHoraire(start, end);
+
+        //When
+        deliveries.setSlot(slot);
+
+        //Then
+        assertEquals(slot, deliveries.getSlot());
+    }
 }
