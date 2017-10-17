6dcc390695d0d5e5d2b72f02c0837867ab35ad38
Nathan Casals <nathan.casals@smartprospective.com>
2017-10-17 09:54:26 +0200

    wip [ihm] : added State and Controller

---
+++ ui/src/main/java/lhexanome/optimodlivraison/ui/controller/Controller.java
@@ -0,0 +1,11 @@
+package lhexanome.optimodlivraison.ui.controller;
+
+/**
+ * Created by nathan on 17/10/17.
+ */
+public class Controller {
+    public void Controller() {
+
+    }
+    public State currentState;
+}
---
+++ ui/src/main/java/lhexanome/optimodlivraison/ui/controller/State.java
@@ -0,0 +1,12 @@
+package lhexanome.optimodlivraison.ui.controller;
+import lhexanome.optimodlivraison.ui.mappreview.MapPreviewActions;
+import lhexanome.optimodlivraison.ui.mappreview.MapShowDemandActions;
+import lhexanome.optimodlivraison.ui.mappreview.OrderEditorAction;
+//import lhexanome.optimodlivraison.ui.welcome.WelcomeController;
+
+/**
+ * Created by nathan on 17/10/17.
+ */
+public interface State {
+
+}
