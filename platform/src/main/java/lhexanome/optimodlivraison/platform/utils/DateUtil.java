427048a05ec250c845021b2c3671efa1fd2e93e8
Marc-Antoine Fernandes <embraser01@gmail.com>
2017-10-27 15:39:01 +0200

    Add time slot in delivery parser

--- platform/src/main/java/lhexanome/optimodlivraison/platform/utils/DateUtil.java
+++ platform/src/main/java/lhexanome/optimodlivraison/platform/utils/DateUtil.java
@@ -18,17 +18,31 @@ public final class DateUtil {
 
     /**
      * static function giving a Date from hour and minute.
-     * @param hours hours given
+     *
+     * @param hours   hours given
      * @param minutes minutes given
      * @return Date wanted
      */
     public static Date getDate(int hours, int minutes) {
-        Date sortie = new Date();
         DateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
         try {
             return simpleDateFormat.parse(hours + ":" + minutes);
         } catch (ParseException e) {
             return null;
         }
+    }
+
+    /**
+     * Parse a string with a certain format.
+     *
+     * @param format Format of the string, {@see SimpleDateFormat}
+     * @param value  String to parse
+     * @return A date object
+     * @throws ParseException if unable to parse
+     */
+    public static Date parseDate(String format, String value) throws ParseException {
+        DateFormat simpleDateFormat = new SimpleDateFormat(format);
+
+        return simpleDateFormat.parse(value);
     }
 }
