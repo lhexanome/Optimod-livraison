5add153cf488d856a69a6e7eeaead191585edb9e
loicsrz <ljedi007@gmail.com>
2017-11-07 15:23:51 +0100

    add RemovedDeliveryCommand and MoveDeliveryCommand

---
+++ platform/src/test/java/lhexanome/optimodlivraison/platform/command/sync/AddDeliveryCommandTest.java
@@ -0,0 +1,20 @@
+package lhexanome.optimodlivraison.platform.command.sync;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddDeliveryCommandTest {
+    @Test
+    void doExecute() {
+    }
+
+    @Test
+    void doUndo() {
+    }
+
+    @Test
+    void doRedo() {
+    }
+
+}
