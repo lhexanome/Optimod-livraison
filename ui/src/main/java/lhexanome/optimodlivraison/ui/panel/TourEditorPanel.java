6053c56333d1011a4c8ebe58454b76a49fbf74bc
HVogel <hugues.vogel@hotmail.fr>
2017-11-07 10:53:44 +0100

    crtl + alt + L

--- ui/src/main/java/lhexanome/optimodlivraison/ui/panel/TourEditorPanel.java
+++ ui/src/main/java/lhexanome/optimodlivraison/ui/panel/TourEditorPanel.java
@@ -11,8 +11,6 @@ import lhexanome.optimodlivraison.ui.edition.DeliveryListTransferHandler;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Vector;
 
 /**
