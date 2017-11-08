45c3557a767e9a2a8f84b2dd605992360888824e
Marc-Antoine Fernandes <embraser01@gmail.com>
2017-11-08 21:10:44 +0100

    Clean up code and create test for async command

--- platform/src/test/java/lhexanome/optimodlivraison/platform/command/ParseDeliveryOrderCommandTest.java
+++ platform/src/test/java/lhexanome/optimodlivraison/platform/command/ParseDeliveryOrderCommandTest.java
@@ -1,23 +1,77 @@
 package lhexanome.optimodlivraison.platform.command;
 
+import lhexanome.optimodlivraison.platform.listeners.ParseDeliveryOrderListener;
+import lhexanome.optimodlivraison.platform.listeners.ParseMapListener;
+import lhexanome.optimodlivraison.platform.models.DeliveryOrder;
+import lhexanome.optimodlivraison.platform.models.RoadMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 class ParseDeliveryOrderCommandTest {
-    private File littleDeliveryOrder;
+    private CountDownLatch lock = new CountDownLatch(1);
+
+    private ParseDeliveryOrderCommand command;
+
 
     @BeforeEach
-    void setup() {
+    void setup() throws InterruptedException {
+        File deliveryOrder = new File(this.getClass().getResource("/xml/deliveries/DLpetit5.xml").getFile());
+        File map = new File(this.getClass().getResource("/xml/map/planLyonPetit.xml").getFile());
 
-        littleDeliveryOrder = new File(this.getClass().getResource("/xml/deliveries/DLpetit5.xml").getFile());
+        final RoadMap[] roadMap = new RoadMap[1];
+
+        ParseMapCommand parseMapCommand = new ParseMapCommand(map);
+        parseMapCommand.setListener(new ParseMapListener() {
+            @Override
+            public void onMapParsed(RoadMap roadMapParsed) {
+                roadMap[0] = roadMapParsed;
+                lock.countDown();
+            }
+
+            @Override
+            public void onMapParsingFail(Exception e) {
+                fail("Couldn't get a road map for ParseDeliveryOrderCommandTest", e);
+            }
+        });
+
+        parseMapCommand.execute();
+
+        lock.await(2, TimeUnit.SECONDS);
+
+        command = new ParseDeliveryOrderCommand(deliveryOrder, roadMap[0]);
     }
 
     @Test
-    void shouldLoadTestFile() {
+    void shouldLoadTestFile() throws InterruptedException {
+        final DeliveryOrder[] deliveryOrder = new DeliveryOrder[1];
+        CountDownLatch lock = new CountDownLatch(1);
 
-//        deliveryFacade.loadDeliveryOrderFromFile(littleDeliveryOrder, roadMap);
+        command.setListener(new ParseDeliveryOrderListener() {
+            @Override
+            public void onDeliveryOrderParsed(DeliveryOrder deliveryOrderParsed) {
+                deliveryOrder[0] = deliveryOrderParsed;
+                lock.countDown();
+            }
+
+            @Override
+            public void onDeliveryOrderParsingFail(Exception e) {
+                fail("Unable to parse", e);
+            }
+        });
+
+        command.execute();
+
+        lock.await(1, TimeUnit.SECONDS);
+
+        assertThat(deliveryOrder[0]).isNotNull();
+        assertThat(deliveryOrder[0].getDeliveries()).hasSize(4);
     }
 
 }
