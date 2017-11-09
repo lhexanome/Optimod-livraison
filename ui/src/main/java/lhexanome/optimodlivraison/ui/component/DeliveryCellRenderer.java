fafff764be0b95de96b51839441dfad639b2365f
Nathan Casals <nathan.casals@smartprospective.com>
2017-11-09 14:17:00 +0100

    Added timeslot warning if timeslot not respected in delivery cell renderer

--- ui/src/main/java/lhexanome/optimodlivraison/ui/component/DeliveryCellRenderer.java
+++ ui/src/main/java/lhexanome/optimodlivraison/ui/component/DeliveryCellRenderer.java
@@ -47,6 +47,11 @@ public class DeliveryCellRenderer implements ListCellRenderer<Delivery> {
     private static final Color WAITING_TIME_COLOR = Color.ORANGE;
 
     /**
+     * warning text color.
+     */
+    private static final Color WARNING_COLOR = Color.RED;
+
+    /**
      * Constructor.
      *
      * @param currentContext Current context.
@@ -92,12 +97,10 @@ public class DeliveryCellRenderer implements ListCellRenderer<Delivery> {
         GroupLayout.ParallelGroup parallelGroup = layout.createParallelGroup();
         GroupLayout.SequentialGroup sequentialGroup = layout.createSequentialGroup();
 
-
         // Index line
         if (hasContext(Context.TOUR)) {
             setIndexLine(font, attributes, index, parallelGroup, layout, sequentialGroup, value);
         }
-
 
         // Time slot line
         TimeSlot timeSlot = value.getSlot();
@@ -105,7 +108,6 @@ public class DeliveryCellRenderer implements ListCellRenderer<Delivery> {
 
             setTimeSlotLine(font, attributes, parallelGroup, layout, sequentialGroup, value);
         }
-
 
         // Duration label
         if (hasContext(Context.DELIVERY_ORDER, Context.TOUR)) {
@@ -119,11 +121,17 @@ public class DeliveryCellRenderer implements ListCellRenderer<Delivery> {
             setWaitingTime(value, font, attributes, parallelGroup, layout, sequentialGroup);
         }
 
+        // TimeSlot warning label
+        if (hasContext(Context.DELIVERY_ORDER, Context.TOUR) &&  value.getSlot() != null
+                && value.getEstimateDate() != null) {
+
+            setTimeSlotWarning(value, font, attributes, parallelGroup, layout, sequentialGroup);
+        }
+
         // Address line
         if (hasContext(Context.DELIVERY_ORDER, Context.TOUR)) {
             setAddressLine(font, attributes, layout, value, parallelGroup, sequentialGroup);
         }
-
 
         layout.setVerticalGroup(sequentialGroup);
         layout.setHorizontalGroup(parallelGroup);
@@ -143,6 +151,44 @@ public class DeliveryCellRenderer implements ListCellRenderer<Delivery> {
         }
 
         return panel;
+    }
+
+    /**
+     * Renders a warning if a timslot constraint is not respected during the tour.
+     * @param value value
+     * @param font font
+     * @param attributes attributes
+     * @param parallelGroup  parallelGroup
+     * @param layout layout
+     * @param sequentialGroup sequentialGroup
+     */
+    private void setTimeSlotWarning(Delivery value, Font font, Map attributes,
+                                    GroupLayout.ParallelGroup parallelGroup, GroupLayout layout,
+                                    GroupLayout.SequentialGroup sequentialGroup) {
+
+        JLabel timeSlotLabel = new JLabel("Plage horaire non respectée.");
+        timeSlotLabel.setFont(font.deriveFont(attributes));
+        timeSlotLabel.setForeground(WARNING_COLOR);
+
+        double lateTime = -getWaitingTime(value);
+        long minutes = getMinutes(lateTime);
+        long seconds = getSeconds(lateTime);
+
+        if (lateTime > 0) {
+            JLabel timeSlotValue = new JLabel(
+                    "Retard de " + String.valueOf(minutes) + " min " + String.valueOf(seconds) + "s"
+            );
+
+            parallelGroup.addGroup(layout.createSequentialGroup()
+                    .addComponent(timeSlotLabel)
+                    .addComponent(timeSlotValue)
+            );
+
+            sequentialGroup.addGroup(layout.createParallelGroup()
+                    .addComponent(timeSlotLabel)
+                    .addComponent(timeSlotValue)
+            );
+        }
     }
 
     /**
@@ -284,29 +330,60 @@ public class DeliveryCellRenderer implements ListCellRenderer<Delivery> {
     private void setWaitingTime(Delivery value, Font font, Map attributes,
                                 GroupLayout.Group parallelGroup, GroupLayout layout,
                                 GroupLayout.Group sequentialGroup) {
-        Date arrival = value.getEstimateDate();
-        Date minimalArrival = value.getSlot().getStart();
-        double waitingTime = -(value.getSlot().getTimescaleBetween(arrival, minimalArrival)
+
+        double waitingTime = getWaitingTime(value);
+
+        long minutes = getMinutes(waitingTime);
+        long seconds = getSeconds(waitingTime);
+
+        if (waitingTime > 0) {
+            JLabel waitingTimeLabel = new JLabel("Temps d'attente :");
+            waitingTimeLabel.setFont(font.deriveFont(attributes));
+            waitingTimeLabel.setForeground(WAITING_TIME_COLOR);
+
+            JLabel waitingTimeValue = new JLabel(String.valueOf(minutes) + " min " + String.valueOf(seconds) + " s");
+            waitingTimeValue.setForeground(WAITING_TIME_COLOR);
+            parallelGroup.addGroup(layout.createSequentialGroup()
+                    .addComponent(waitingTimeLabel)
+                    .addComponent(waitingTimeValue)
+            );
+
+            sequentialGroup.addGroup(layout.createParallelGroup()
+                    .addComponent(waitingTimeLabel)
+                    .addComponent(waitingTimeValue)
+            );
+        }
+    }
+
+    /**
+     * returns the 2d part of the waiting time in seconds <= 60 seconds.
+     * @param waitingTime waiting time
+     * @return number of waiting time seconds.
+     */
+    private long getSeconds(double waitingTime) {
+        return (long) Math.floor((waitingTime - getMinutes(waitingTime)) * 60);
+    }
+
+    /**
+     * returns number of waiting time minutes.
+     * @param waitingTime waiting time
+     * @return number of waiting time minutes.
+     */
+    private long getMinutes(double waitingTime) {
+        return (long) Math.floor(waitingTime);
+    }
+
+    /**
+     * returns the waiting time of a delivery during the tour.
+     * @param delivery the delivery
+     * @return waiting time in seconds
+     */
+    private double getWaitingTime(Delivery delivery) {
+        Date arrival = delivery.getEstimateDate();
+        Date minimalArrival = delivery.getSlot().getStart();
+        double waitingTime = -(delivery.getSlot().getTimescaleBetween(arrival, minimalArrival)
                 / (double) MILLISECONDS_TO_SECONDS);
-
-        long minutes = (long) Math.floor(waitingTime);
-        long seconds = (long) Math.floor((waitingTime - minutes) * 60);
-
-        JLabel waitingTimeLabel = new JLabel("Temps d'attente :");
-        waitingTimeLabel.setFont(font.deriveFont(attributes));
-        waitingTimeLabel.setForeground(WAITING_TIME_COLOR);
-
-        JLabel waitingTimeValue = new JLabel(String.valueOf(minutes) + " min " + String.valueOf(seconds) + " s");
-        waitingTimeValue.setForeground(WAITING_TIME_COLOR);
-        parallelGroup.addGroup(layout.createSequentialGroup()
-                .addComponent(waitingTimeLabel)
-                .addComponent(waitingTimeValue)
-        );
-
-        sequentialGroup.addGroup(layout.createParallelGroup()
-                .addComponent(waitingTimeLabel)
-                .addComponent(waitingTimeValue)
-        );
+        return waitingTime;
     }
 
     /**
