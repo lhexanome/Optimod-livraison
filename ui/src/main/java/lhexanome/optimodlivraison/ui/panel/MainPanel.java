dab6960fb2a44270b16e7ef7677119c275a11ad3
Egaro555 <hugues.vogel@hotmail.fr>
2017-11-10 09:34:38 +0100

    Update MainPanel.java

--- ui/src/main/java/lhexanome/optimodlivraison/ui/panel/MainPanel.java
+++ ui/src/main/java/lhexanome/optimodlivraison/ui/panel/MainPanel.java
@@ -6,11 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 /**
  * Main panel of the application.
