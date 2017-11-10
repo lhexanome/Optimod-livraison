42b613b9b8acb434457d3340f893c1fe656e87c3
Unknown <hugues.vogel@hotmail.fr>
2017-11-10 17:40:09 +0100

    reformat

--- platform/src/test/java/lhexanome/optimodlivraison/platform/compute/SimplifiedMapTest.java
+++ platform/src/test/java/lhexanome/optimodlivraison/platform/compute/SimplifiedMapTest.java
@@ -1,7 +1,6 @@
 package lhexanome.optimodlivraison.platform.compute;
 
 import lhexanome.optimodlivraison.platform.command.ParseMapCommand;
-import lhexanome.optimodlivraison.platform.listeners.ParseDeliveryOrderListener;
 import lhexanome.optimodlivraison.platform.listeners.ParseMapListener;
 import lhexanome.optimodlivraison.platform.models.Delivery;
 import lhexanome.optimodlivraison.platform.models.DeliveryOrder;
@@ -11,7 +10,6 @@ import lhexanome.optimodlivraison.platform.models.Path;
 import lhexanome.optimodlivraison.platform.models.RoadMap;
 import lhexanome.optimodlivraison.platform.models.Vector;
 import lhexanome.optimodlivraison.platform.models.Warehouse;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -75,18 +73,18 @@ class SimplifiedMapTest {
         demande_e7d8d5.setWarehouse(e7);
         demande_e7d8d5.addDelivery(d5);
         demande_e7d8d5.addDelivery(d8);
-        sm_e7d8d5 = new SimplifiedMap(demande_e7d8d5,roadMap);
+        sm_e7d8d5 = new SimplifiedMap(demande_e7d8d5, roadMap);
     }
 
     @Test
-    public void testDijkstra(){
+    public void testDijkstra() {
 
         DeliveryOrder demande = new DeliveryOrder();
         demande.setWarehouse(e7);
         demande.addDelivery(d8);
         demande.addDelivery(d5);
 
-        SimplifiedMap sm = new SimplifiedMap(demande,roadMap);
+        SimplifiedMap sm = new SimplifiedMap(demande, roadMap);
         sm.computeGraph();
 
         //TODO real assert
@@ -94,33 +92,33 @@ class SimplifiedMapTest {
     }
 
     @Test
-    public void testComputeGraphWidthOutDelivery(){
+    public void testComputeGraphWidthOutDelivery() {
         SimplifiedMap simplifiedMapTested = new SimplifiedMap(roadMap);
         try {
             simplifiedMapTested.computeGraph();
             fail("compute no failed widthOut delivery");
-        }catch (Exception e) {
+        } catch (Exception e) {
 
         }
 
     }
 
     @Test
-    public void testComputeGraph(){
+    public void testComputeGraph() {
         sm_e7d8d5.computeGraph();
         Map<Halt, ArrayList<Path>> res = sm_e7d8d5.getGraph();
 
         Map<Halt, Path[]> predicat = new HashMap<>();
         new ArrayList<>();
-        predicat.put(e7,new Path[]{
+        predicat.put(e7, new Path[]{
                 interssectionsIdToPath(e7, d5, roadMap, 7l, 6L, 5L),
                 interssectionsIdToPath(e7, d8, roadMap, 7l, 9L, 8L)
         });
-        predicat.put(d8,new Path[]{
+        predicat.put(d8, new Path[]{
                 interssectionsIdToPath(d8, d5, roadMap, 8l, 6L, 5L),
                 interssectionsIdToPath(d8, e7, roadMap, 8l, 9L, 7L)
         });
-        predicat.put(d5,new Path[]{
+        predicat.put(d5, new Path[]{
                 interssectionsIdToPath(d5, d8, roadMap, 5l, 10L, 8L),
                 interssectionsIdToPath(d5, e7, roadMap, 5l, 6L, 7L)
         });
@@ -130,7 +128,7 @@ class SimplifiedMapTest {
     }
 
     @Test
-    public void testShortestPathList(){
+    public void testShortestPathList() {
         Set<Halt> alts = new HashSet<>();
         alts.add(d5);
         alts.add(d8);
@@ -145,18 +143,18 @@ class SimplifiedMapTest {
     }
 
     @Test
-    public void testShortestPathUnrechable(){
+    public void testShortestPathUnrechable() {
         try {
             Path res = sm_e7d8d5.shortestPathList(e7, d13);
             fail("return result without exeption");
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
 
     }
 
     @Test
-    public void testShortestPath(){
+    public void testShortestPath() {
 
         Path res = sm_e7d8d5.shortestPathList(e7, d5);
 
@@ -166,21 +164,21 @@ class SimplifiedMapTest {
     }
 
 
-    public Vector findVector(RoadMap map, Intersection depart, Intersection arriver){
-        for(Vector v : map.getVectorsFromIntersection(depart))
-            if(v.getDestination() == arriver)
+    public Vector findVector(RoadMap map, Intersection depart, Intersection arriver) {
+        for (Vector v : map.getVectorsFromIntersection(depart))
+            if (v.getDestination() == arriver)
                 return v;
         return null;
     }
 
-    public Path interssectionsIdToPath(Halt start, Halt end, RoadMap roadMap, long ... intersectionsId){
-        Path p = new Path(start,end);
+    public Path interssectionsIdToPath(Halt start, Halt end, RoadMap roadMap, long... intersectionsId) {
+        Path p = new Path(start, end);
         Intersection[] intersections = new Intersection[intersectionsId.length];
-        for(int i = 0; i < intersectionsId.length; i++) {
+        for (int i = 0; i < intersectionsId.length; i++) {
             intersections[i] = roadMap.findIntersectionById(intersectionsId[i]);
         }
-        for(int i = 1; i < intersections.length; i++)
-            p.addVector(findVector(roadMap, intersections[i-1], intersections[i]));
+        for (int i = 1; i < intersections.length; i++)
+            p.addVector(findVector(roadMap, intersections[i - 1], intersections[i]));
 
         return p;
     }
